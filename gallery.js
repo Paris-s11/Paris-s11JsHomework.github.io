60fed02352e45501c154353e7f32f6505b43a8ee
Paris-s11 <moazenip1375@gmail.com>
2024-09-12 11:33:19 -0700

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,15 @@
+function upDate(previewPic){
+    x = document.getElementById("image");
+    x.style.backgroundImage = "url('"+ previewPic.src +"')";
+   
+    x.innerHTML = previewPic.alt ;
+    
+      };
+  
+      function unDo(){
+        x = document.getElementById("image");
+        x.style.backgroundImage = "none" ;
+
+        x.innerHTML = "Hover over an image below to display here."
+          
+      }
